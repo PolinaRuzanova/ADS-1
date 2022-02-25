cff6a679010b8048f73ef3dcdafaa6b7ddc58105
PolinaRuzanova <94633834+PolinaRuzanova@users.noreply.github.com>
2022-02-25 17:02:02 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,16 +5,46 @@
 
 bool checkPrime(uint64_t value) {
 // вставьте код функции
+  for (int i = 2; i < value; i++) {
+        if (value % i == 0) {
+            return false;
+            break;
+        }
+    }
+   return true;
 }
 
 uint64_t nPrime(uint64_t n) {
 // вставьте код функции
+  int count = 0;
+    for (int i = 1; i > 0; i++) {
+        if (checkPrime(i) ==true) {
+            count++;
+            if (count == n) {
+                return i;
+                break;
+            }
+        }
+    }
 }
 
 uint64_t nextPrime(uint64_t value) {
 // вставьте код функции
+  value++;
+    if (checkPrime(value) == true) {
+        return value;
+    }
+    return nextPrime(value);
 }
 
 uint64_t sumPrime(uint64_t hbound) {
 // вставьте код функции
+  int sum = 0;
+    while (hbound !=0) {
+        hbound--;
+        if (checkPrime(hbound) == true) {
+            sum = sum + hbound;
+        }
+    }
+    return sum;
 }
