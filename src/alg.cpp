9fd4156f62b147def185f1ce543a93abe2446326
PolinaRuzanova <94633834+PolinaRuzanova@users.noreply.github.com>
2022-02-25 19:32:52 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -10,8 +10,8 @@ bool checkPrime(uint64_t value) {
             return false;
             break;
         }
-   }
-   return true;
+    }
+    return true;
 }
 
 uint64_t nPrime(uint64_t n) {
@@ -39,7 +39,7 @@ uint64_t nextPrime(uint64_t value) {
 uint64_t sumPrime(uint64_t hbound) {
 // вставьте код функции
   uint64_t sum = 0;
-    while (hbound !=0) {
+    while (hbound !=2) {
         hbound--;
         if (checkPrime(hbound) == true) {
             sum = sum + hbound;
